657806fd029d3b71a91aa30e61a70212798dc625
Jason Kolenosky <jasonk58@gmail.com>
2020-10-16 13:31:38 -0700

    Get webpack setup and a test page up and running

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import '../styles/home.css'
+
+class Home extends Component {
+    constructor(props){
+      super(props)
+      this.state = {
+      }
+    }
+
+    render() {
+      return (
+        <div id='HomeWrapper'>
+          Hello World
+        </div>
+      )
+    }
+}
+
+export default Home;
